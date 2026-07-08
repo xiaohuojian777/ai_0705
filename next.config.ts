59a04c40315f6b1d99a7947d0ea104709ddc6da3
wf <test@123.com>
2026-07-08 14:57:28 +0800

    fix_vercel_0709

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/**/*": ["./prisma/**/*"],
+  },
+};
 
 export default nextConfig;
